96e10fd5683654fd7a5464c38b870b144b15b607
Wenxiang Ji <879274904@qq.com>
2021-02-08 21:53:00 +0800

    index is finished

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,11 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    // 全局统一如果没有token值，跳转至login页面，通过complete完成
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 || !localStorage.getItem('token')) {
+            location.href = '/login.html'
+            localStorage.removeItem('token')
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+const layer = layui.layer
+
+// 发起请求，获取用户信息，通过得到的信息来切换显示用户的头像和昵称
+getUserInfo()
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success(res) {
+            console.log(res);
+            if (res.status !== 0) return layer.msg('获取失败！')
+            // layer.msg('获取成功！')
+            // 获取成功后，将信息渲染到页面
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 渲染页面方法
+function renderAvatar(data) {
+    // 声明变量存储用户名，如果有昵称就写上昵称，如果没有，就写上用户名
+    const wel = data.nickname || data.username
+    $('#welcome').html('欢迎&nbsp&nbsp' + wel)
+
+    // 首先判断有没有图片头像，没有就用文本头像
+    // 将tex-avatar 中写入昵称或用户名的首字母，大写
+    if (data.user_pic) {
+        $('.tex-avatar').html(wel[0].toUpperCase())
+        $('.pic-avatar').hide()
+    } else {
+        $('.tex-avatar').hide()
+        $('.pic-avatar').attr('src', data.user_pic)
+    }
+}
+
+// 退出功能
+$('.tuichu').on('click', function () {
+    // 跳出提示框，清空token，跳转至login页面
+    layer.confirm('确认退出?', { icon: 3, title: '提示' }, function (index) {
+        //do something
+        location.href = '/login.html'
+        localStorage.removeItem('token')
+        layer.close(index);
+    });
+
+})
